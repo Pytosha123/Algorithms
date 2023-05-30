868ef3964183f8e0db08b55a8d51a6702f0a208b
Anna Tumanova <pytosha@gmail.com>
2023-05-30 14:41:18 +0300

    Seminar_2

---
+++ Seminar_2/dz_1.py
@@ -0,0 +1,43 @@
+#**
+ #* Реализовать сортировку подсчетом.
+ #* 
+ #*/
+#static void countingSort(int[] array) {
+
+def countsort(A, k):
+
+	
+	output = [0] * len(A)
+
+	freq = [0] * (k + 1)
+
+	
+	for i in A:
+		freq[i] = freq[i] + 1
+
+	
+	total = 0
+	for i in range(k + 1):
+		oldCount = freq[i]
+		freq[i] = total
+		total += oldCount
+
+	
+	for i in A:
+		output[freq[i]] = i
+		freq[i] = freq[i] + 1
+
+	
+	for i in range(len(A)):
+		A[i] = output[i]
+
+
+if __name__ == '__main__':
+
+	A = [4, 2, 10, 10, 1, 4, 2, 1, 10]
+
+	
+	k = 10
+
+	countsort(A, k)
+	print(A)
